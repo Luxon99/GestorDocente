50c2962fd3a256e920d0e5a8090ffe2941341695
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-04-18 13:53:45 -0400

    Update StudentDTO.java
    creado sus atributos y seters y getters y el constructor del Student DTO

--- GestionDocente/src/gestion_docente/dto/StudentDTO.java
+++ GestionDocente/src/gestion_docente/dto/StudentDTO.java
@@ -6,9 +6,65 @@ package gestion_docente.dto;
 
 /**
  *
- * @author César Fernández García
- * Esta clase permite la manipulación de la tabla student
+ * @author César Fernández García Esta clase permite la manipulación de la tabla
+ * student
  */
 public class StudentDTO {
+
+    private int id_student;
+    private String name;
+    private String surnames;
+    private boolean sex;
+    private String municipality;
+
+    public int getId_student() {
+        return id_student;
+    }
+
+    public void setId_student(int id_student) {
+        this.id_student = id_student;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getMunicipality() {
+        return municipality;
+    }
+
+    public void setMunicipality(String municipality) {
+        this.municipality = municipality;
+    }
+
+    public StudentDTO(int id_student, String name, String surnames, boolean sex, String municipality) {
+        this.id_student = id_student;
+        this.name = name;
+        this.surnames = surnames;
+        this.sex = sex;
+        this.municipality = municipality;
+    }
+    
+    
     
 }
