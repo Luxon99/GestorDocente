48a829dc0439ca8739b2dfbf499822fd5773e46c
Cesar0112 <hectorfernandezdiaz6@gmail.com>
2023-04-22 23:31:51 -0400

    Metodo de mostrar paneles

--- GestionDocente/src/gestion_docente/utils/Conection.java
+++ GestionDocente/src/gestion_docente/utils/Conection.java
@@ -3,12 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package gestion_docente.utils;
-
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author César Fernández García
  * Esta clase define la conexion a la base de datos
  */
 public class Conection {
-    
+
+    private static java.sql.Connection connection;
+
+    public Conection(String serveraddres, String database, String user, String pass) throws ClassNotFoundException, SQLException {
+
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://" + serveraddres + ":5433/" + database;
+        connection = DriverManager.getConnection(url, user, pass);
+    }
+
+    public java.sql.Connection getConnection() {
+        return connection;
+    }
 }
